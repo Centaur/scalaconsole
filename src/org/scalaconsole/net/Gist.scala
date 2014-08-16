1d094491dbdd9dc27f4fbb271da8e6a744b74cbb
Centaur <heliu.smth@gmail.com>
2014-08-16 11:15:15 +0800

    fix #14, finally

--- src/org/scalaconsole/net/Gist.scala
+++ src/org/scalaconsole/net/Gist.scala
@@ -1,13 +1,13 @@
 package org.scalaconsole.net
 
 import java.net._
+import javafx.scene.input.{Clipboard, ClipboardContent}
+
 import com.google.gson.JsonParser
-import javafx.scene.input.{ClipboardContent, Clipboard}
 import org.scalaconsole.fxui.FxUtil
-import java.io.DataInputStream
 
 object Gist {
-  def post(content: String, accessToken: Option[String], description: String = "Post By ScalaConsole"):String = {
+  def post(content: String, accessToken: Option[String], description: String = "Post By ScalaConsole"): String = {
 
     val x = new URL("https://api.github.com/gists" + accessToken.fold("")("?access_token=" + _ + "&scope=gist"))
     val conn = x.openConnection.asInstanceOf[HttpURLConnection]
@@ -17,13 +17,17 @@ object Gist {
     conn.setUseCaches(false)
     conn.setRequestMethod("POST")
     conn.setRequestProperty("Content-type", "text/json; charset=UTF-8")
-    conn.connect()
     val requestJSON = """{
     "description": "%s",
     "public": true,
     "files": {
       "fromScalaConsole.scala": {"content": "%s"}
-    }}""".format(description, content.replaceAll( """\\""", """\\\\""").replaceAll( """\"""", """\\"""").replaceAll("\n", "\\\\n"))
+    }}""".format(description,
+                 content
+                   .replaceAll( """\\""", """\\\\""")
+                   .replaceAll( """\"""", """\\"""")
+                   .replaceAll("\n", "\\\\n")
+                   .replaceAll("\r", "\\\\r"))
 
     conn.getOutputStream.write(requestJSON.getBytes("UTF-8"))
 
@@ -36,8 +40,8 @@ object Gist {
         copyContent.putString(url)
 
         FxUtil.onEventThread {
-          Clipboard.getSystemClipboard.setContent(copyContent)
-        }
+                               Clipboard.getSystemClipboard.setContent(copyContent)
+                             }
 
         "New Gist created at %s. URL has been copied to clipboard.".format(url)
       case _ =>
